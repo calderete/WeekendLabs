95bd311a50f687e37aacdbd8a2a8ef1fbd0d766e
Charles <codprestige10@gmail.com>
2015-10-04 14:42:39 -0400

    unit testing with no errors

--- test.rb
+++ test.rb
@@ -1,9 +1,25 @@
 require 'minitest/autorun'
 require './t3game'
-require './t3player'
+require './oo_tic_tac_toe'
+require 'pry'
 
 class GameTest < Minitest::Test
-	def test_take_turn
-		assert @player_.player_marks
+	def set_up
+		
 	end
+
+	def test_game_over
+		board = (1..9).to_a
+		game = Game.new
+		refute game.game_over(board)
+		refute game.draw(board)
+	end
+
+	def test_win
+		board = ["X", "X", "X", 3, 4, 5, 6, 7, 8,]
+		game = Game.new
+		assert game.win?(board)
+	end
+
 end
+
